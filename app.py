87d7b31433355c3d9eb03951ec6187623f8743ea
Abdelrahman Ashraf <abdelrahmanashraf25102000@gmail.comm>
2022-03-19 20:45:27 +0200

    Scrape Twitter Data

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from config import engine
-from models import Base, Tweet
 from sqlalchemy.orm import sessionmaker
-import tweepy
-from tweepy import OAuthHandler
+import tweepy as tw
 import pandas as pd
 import time
 from dotenv import load_dotenv
@@ -20,11 +17,22 @@ api_secret = os.environ.get("api_secret")
 access_token = os.environ.get("access_token")
 access_token_secret = os.environ.get("access_token_secret")
 
-auth = tweepy.OAuthHandler(api_key, api_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+auth = tw.OAuthHandler(api_key , api_secret)
+auth.set_access_token(access_token , access_token_secret)
+
+api = tw.API(auth , wait_on_rate_limit=True)
+
+search_words = "#amazon"
+date_since = "2020-2-17"
+
+# Collect tweets
+tweets = tw.Cursor(api.search_tweets ,
+              q=search_words,
+              lang="en",
+              since=date_since).items(1000)
 
 
-
+for tweet in tweets:
+    print(tweet.text)
 
 
--- config.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-
-# Connect to the database
-DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/twitter_scrapper'
-engine = create_engine(DATABASE_URI)
